6d2d863b5656203518c6e782e549a3a7885eabcb
T. Sirisha <reddysirisa2000@gmail.com>
2024-10-04 13:09:53 +0530

    my personal website

---
+++ assets/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Show thank you message after 5 seconds
+    setTimeout(function() {
+        document.querySelector('.thank-you').style.display = 'block';
+    }, 5000);
+});
